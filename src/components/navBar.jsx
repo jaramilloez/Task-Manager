e1829b8158de3ab96b76c8faf53802b10dda317c
Ezekiel Jaramillo <jaramilloez2004@gmail.com>
2024-05-26 14:36:49 -0600

    Add title and navLink classes

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from '../utility/commonStyles';
 
 const NavBar = () => {
     return (
-        <nav className='bg-dark'>
+        <nav className='bg-light d-flex justify-content-between align-items-center'>
             <div>
-
+                My To Dos
             </div>
-            <ul className='d-flex'>
-                <li>
+            <ul className='d-flex m-0 list-unstyled'>
+                <li className={ `${styles.navLink}` }>
                     <Link to="/">Tasks</Link>
                 </li>
-                <li>
+                <li className={ `${styles.navLink}` }>
                     <Link to="/login">Login</Link>
                 </li>
-                <li>
-                    <Link tp='/admin'>Admin</Link>
+                <li className={ `${styles.navLink}` }>
+                    <Link to='/admin'>Admin</Link>
                 </li>
             </ul>
         </nav>
